852fbda876aee0b58c0e373684c0a09c5937fef7
sonickmumba <kingellie.mumba@gmail.com>
2022-07-27 12:48:35 +0200

    Add the navigation href link to mobile-link selector

--- main.js
+++ main.js
@@ -3,21 +3,19 @@ const menuBar = document.querySelector('.menu-nav-mobile #menu');
 const headline = document.querySelector('.headline-section');
 const body = document.querySelector('body');
 
-menuBar.addEventListener('click', (e) => {
-  e.preventDefault();
+menuBar.addEventListener('click', () => {
   menuNavMobile.classList.toggle('show');
   body.classList.toggle('show');
   headline.classList.toggle('show');
   menuBar.classList.toggle('show');
 });
 
-const mobileLinks = document.querySelectorAll('.mobile-link');
+const mobileLinks = document.querySelectorAll('.mobile-link a');
 Array.from(mobileLinks).forEach((link) => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    menuNavMobile.classList.toggle('show');
-    body.classList.toggle('show');
-    headline.classList.toggle('show');
-    menuBar.classList.toggle('show');
+  link.addEventListener('click', () => {
+    menuNavMobile.classList.remove('show');
+    body.classList.remove('show');
+    headline.classList.remove('show');
+    menuBar.classList.remove('show');
   });
 });
