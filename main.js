a7871b59755424a1d5b6f31fed5028e154e0c1f1
sonickmumba <kingellie.mumba@gmail.com>
2022-07-27 08:48:45 +0200

    Add main.js file

---
+++ main.js
@@ -0,0 +1,12 @@
+const menuNavMobile = document.querySelector('.menu-nav-mobile');
+const menuBar = document.querySelector('.menu-nav-mobile #menu');
+const headline = document.querySelector('.headline-section');
+const body = document.querySelector('body');
+
+menuBar.addEventListener('click', (e) => {
+  e.preventDefault();
+  menuNavMobile.classList.toggle('show');
+  body.classList.toggle('show');
+  headline.classList.toggle('show');
+  menuBar.classList.toggle('show');
+});
